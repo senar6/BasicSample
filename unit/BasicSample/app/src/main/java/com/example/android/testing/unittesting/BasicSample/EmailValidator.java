e5a6ad0f593f65fd7fb60cf661dae11b131e71e5
senthil <senar.v@gmail.com>
2017-12-15 08:46:03 +0530

    Test commit

--- unit/BasicSample/app/src/main/java/com/example/android/testing/unittesting/BasicSample/EmailValidator.java
+++ unit/BasicSample/app/src/main/java/com/example/android/testing/unittesting/BasicSample/EmailValidator.java
@@ -12,7 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * Modified By vsk for testing Github
+ * Modified By vsk for testing Github testing
  */
 
 package com.example.android.testing.unittesting.BasicSample;
